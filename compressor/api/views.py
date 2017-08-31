b97561dc6abe23c67eed7e4da7fd5e2312c517ff
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-08-31 17:25:01 -0400

    Server is up and runninng using a random URL
    Will change the urls & views later. This works as a placeholder for now

---
+++ compressor/api/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
