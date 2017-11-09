3fb4c0624ddca9ef69b5fa612fa9e5f3a643865c
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-09 16:50:18 -0500

    I think I was overthinking it a bit - this lib is easy

--- compressor/Server/Compressor/Compressor.py
+++ compressor/Server/Compressor/Compressor.py
@@ -1,11 +1,12 @@
 from pydub import effects
 from pydub import AudioSegment
+import pydub
 
 # seg Object:
 # Max Amp: Float variable around 32768.0
 # Frame Count: HZ frequency of Audio - usually 44.1 unless 48k was used
 #
-track = AudioSegment.from_wav("C:\Users\wyatt\Downloads\StillYoung-WhiteLabel(IsThisLove).wav")
+track = AudioSegment.from_wav("StillYoung-WhiteLabel(IsThisLove).wav")
 
 # seg is the track - or each AudioSegment object that is streamed while compressed
 # def compress(seg):
@@ -19,5 +20,5 @@ track = AudioSegment.from_wav("C:\Users\wyatt\Downloads\StillYoung-WhiteLabel(Is
 compressed = effects.compress_dynamic_range(seg=track, threshold=-20.0, ratio = 3.0,
                                     attack = 10.0, release = 100.0)
 
-export = AudioSegment.export(compressed, format="mp3")
-export.write("Compressed.mp3")
+compressed.export("compressed.mp3", format="mp3")
+
