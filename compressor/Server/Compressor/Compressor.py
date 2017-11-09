fc0478f831b6294c8bfd770e01c3840d77e56277
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-09 17:11:02 -0500

    Working ... now to pass audio bits at x rate through to compressor

--- compressor/Server/Compressor/Compressor.py
+++ compressor/Server/Compressor/Compressor.py
@@ -1,24 +1,22 @@
 from pydub import effects
 from pydub import AudioSegment
-import pydub
 
 # seg Object:
 # Max Amp: Float variable around 32768.0
 # Frame Count: HZ frequency of Audio - usually 44.1 unless 48k was used
 #
-track = AudioSegment.from_wav("StillYoung-WhiteLabel(IsThisLove).wav")
-
 # seg is the track - or each AudioSegment object that is streamed while compressed
-# def compress(seg):
-#     compressed = effects.compress_dynamic_range(seg, threshold = -20.0, ratio = 3.0,
-#                                    attack = 10.0, release = 100.0)
-#     export = AudioSegment.export(compressed, format="mp3")
-#     export.write("Compressed.mp3")
+def compress(seg):
+    fileName = "StillYoung-WhiteLabel(IsThisLove).wav"
+    track = AudioSegment.from_wav(fileName)
+    # returns AudioSegment object
+    compressed = effects.compress_dynamic_range(track, threshold = -20.0, ratio = 3.0,
+                                   attack = 10.0, release = 100.0)
+
+    compressed.export('(%s)_Pydub_compressed.pdf' % fileName, format="mp3")
+    print track.get_array_of_samples()
 
 
-# returns AudioSegment object
-compressed = effects.compress_dynamic_range(seg=track, threshold=-20.0, ratio = 3.0,
-                                    attack = 10.0, release = 100.0)
 
-compressed.export("compressed.mp3", format="mp3")
+
 
