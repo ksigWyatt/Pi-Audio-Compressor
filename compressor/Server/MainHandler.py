3dd708471c82ecf5d736418b82a6886418d0f93b
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-22 00:41:12 -0500

    Ahhhhhhhhhh

--- compressor/Server/MainHandler.py
+++ compressor/Server/MainHandler.py
@@ -1,18 +1,36 @@
 import tornado.ioloop
 import tornado.web
 import pyaudio
-import sys
-import os
+import wave
 from pydub import effects
 from pydub import AudioSegment
+import numpy
+import struct
+import math
+import audioop
+import decimal
 
 def compress(seg):
     print "Compressing"
-    chunk = AudioSegment.from_wav(seg)
+    chunk = seg
     # returns AudioSegment object
     compressed = effects.compress_dynamic_range(chunk, threshold=-20.0, ratio=3.0,
                                                 attack=10.0, release=100.0)
     return compressed
+
+# get decibel levels
+def rms( data ):
+    count = len(data)/2
+    format = "%dh"%(count)
+    shorts = struct.unpack( format, data )
+    sum_squares = 0.0
+    for sample in shorts:
+        n = sample * (1.0/32768)
+        sum_squares += n*n
+
+    value = math.sqrt( sum_squares / count )
+    return value
+
 
 def record_and_compress():
     # chunks are recordings of 1024 bytes of data
@@ -21,12 +39,13 @@ def record_and_compress():
     audio_format = pyaudio.paInt16
     channels = 2
     sample_rate = 44100  # in Hz
-    # Set the record time to be 3 minutes that's about the length of a song
-    recording_length = 10
 
+    # Set the record time to be 3 minutes that's about the length of a song
+    recording_length = 60
     p = pyaudio.PyAudio()
 
-    stream = p.open(format=p.get_format_from_width(sample_width),
+    # Stream object <type 'instance'>
+    stream = p.open(format=audio_format,
                     channels=channels,
                     rate=sample_rate,
                     input=True,
@@ -34,12 +53,55 @@ def record_and_compress():
                     frames_per_buffer=chunk)
 
     print("* recording")
+    frames = []
 
     # for all the chunks that are in the array - stream them for compression
     for i in range(0, int(sample_rate / chunk * recording_length)):
+        # data = samples from the stream <type 'str'>
         data = stream.read(chunk)
-        stream.write(data, chunk)
-        compressed = compress(data)
+        audio_levels = audioop.rms(data, 2)
+
+        #print value -- crashes if the value is == 0 so we must catch this
+        if (audio_levels == 0):
+            audio_levels = 1
+            decibels = 20 * math.log10(audio_levels)
+        else:
+            decibels = 20 * math.log10(audio_levels)
+        print decibels
+
+        # if decibels + 30 >= 100:
+        # print "Over 100 detected"
+        print "Creating compressed file"
+        wave_file = wave.open(f="compress.wav", mode="wb")
+        wave_file.writeframes(data)
+        frame_rate = wave_file.getframerate()
+        print frame_rate
+        wave_file.setnchannels(2)
+
+        # Create the proper file
+        compressed = AudioSegment.from_raw(wave_file)
+        compressed.max_possible_amplitude = 32768  # this is very important
+        compressed.frame_rate = frame_rate
+        compressed.channels = 2
+        compressed.sample_width = 2
+
+        post_compression = compress(compressed)
+            #     Send to the compressor
+
+        # wave_data = AudioSegment.from_raw(data, sample_width=sample_width, frame_rate=sample_rate, channels=channels)
+        # print type(wave_data)
+
+        # This might work?
+        # decoded = decimal.Decimal(numpy.fromstring(data, 'Float32'))
+        # print decoded[0]
+
+
+        # sound = AudioSegment.from_wav(file = decoded)
+
+
+        # <NoneType>
+        # stream.write(data, chunk))
+        # compressed = compress(data)
 
     # print compressed
     print("* done")
