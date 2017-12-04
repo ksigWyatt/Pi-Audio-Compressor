2c076d71585a40cb676ddfc1ce5decdf34d6bdb4
ApplianceJohn <thednbgremlin@gmail.com>
2017-12-04 03:41:50 +0000

    Basic user interface
    HTML and script now have support for loading and re-loading the page with new compressor parameters using HTML form submission data.

--- compressor/Server/MainHandler.py
+++ compressor/Server/MainHandler.py
@@ -9,35 +9,38 @@ import struct
 import math
 import audioop
 
-def compress(seg):
+
+def compress(seg, w, x, y, z):
     chunk = seg
-    compressed = effects.compress_dynamic_range(chunk, threshold=-20.0, ratio=3.0, attack=5.0, release=10.0)
+    compressed = effects.compress_dynamic_range(chunk, w, x, y, z)
     return compressed
 
+
 # get decibel levels
-def rms( data ):
-    count = len(data)/2
-    format = "%dh"%(count)
-    shorts = struct.unpack( format, data )
+def rms(data):
+    count = len(data) / 2
+    format = "%dh" % (count)
+    shorts = struct.unpack(format, data)
     sum_squares = 0.0
     for sample in shorts:
-        n = sample * (1.0/32768)
-        sum_squares += n*n
-    value = math.sqrt( sum_squares / count )
+        n = sample * (1.0 / 32768)
+        sum_squares += n * n
+    value = math.sqrt(sum_squares / count)
     return value
 
 
-def record_and_compress():
-
+def record_and_compress(w, x, y, z):
+    # chunks are recordings of 2048 bytes of data
     # This should be a few MB so that the system can capture the samples in a large enough structure.
     # If the chunks are too small then the computer will throw an Overflowed IOError because it cannot store that many
-    chunk = 8192
+
+    chunk = 2048
     sample_width = 2
     audio_format = pyaudio.paInt16
     channels = 1  # Mono - workaround for IOError: [Errno -9981] Input overflowed
-    sample_rate = 16000  # in Hz
+    sample_rate = 44100  # in Hz
 
-    # Set the record time to be 10 seconds -- We are limited to that length because of the Pi
+    # Set the record time to be 3 minutes that's about the length of a song
     recording_length = 60
     p = pyaudio.PyAudio()
 
@@ -66,21 +69,32 @@ def record_and_compress():
             decibels = 20 * math.log10(audio_levels)
 
             # dB 0 < x < 100 -- Normal & Acceptable use
-            not_clipping = (decibels >= 0 and decibels  < 90)
+            not_clipping = (decibels >= 0 and decibels + 50 < 100)
             if not_clipping:
                 stream.write(data, chunk)
 
             # x >= 100
             else:
+                chunk_temp = chunk
 
                 # do your bidding sir
-                sound = AudioSegment(data, sample_width=sample_width, channels=channels, frame_rate=sample_rate)
+                wave_file = wave.open(f="compress.wav(%s)" % i, mode="wb")
+                wave_file.setnchannels(channels)
+                wave_file.setsampwidth(sample_width)
+                wave_file.setframerate(sample_rate)
+                wave_file.writeframesraw(data)
+                wave_file.close()
+
+                # Create the proper file
+                compressed = AudioSegment.from_wav("compress.wav(%s)" % i)
+                os.remove("compress.wav(%s)" % i)  # delete it quickly
 
                 # Send to the compressor
-                post_compression_data = compress(sound) # Type <class 'pydub.audio_segment.AudioSegment'>
+                post_compression_data = compress(compressed, w, x, y,
+                                                 z)  # Type <class 'pydub.audio_segment.AudioSegment'>
 
                 # Stream to the speakers after the
-                stream.write(post_compression_data.raw_data, chunk) # not fluid but it works for me
+                stream.write(post_compression_data.raw_data, chunk_temp)  # not fluid but it works for me
 
     print("* done")
 
@@ -89,19 +103,30 @@ def record_and_compress():
 
     p.terminate()
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         homePage = open("Interface.html", "r")
         htmlCode = homePage.read()
         self.write(htmlCode)
+        self.finish()
+
+    # runs compression after the page loads fully
+    def on_finish(self):  # checks the url for form arguments
+        w = self.get_argument("thrs", 0)
+        x = self.get_argument("rtio", 1)
+        y = self.get_argument("attk", 0.1)
+        z = self.get_argument("rele", 0.1)
+        record_and_compress(float(w), float(x), float(y), float(z))
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
 
+
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8888) # Listen on this port of 127.0.0.1
-    record_and_compress()
+    app.listen(8888)  # Listen on this port of 127.0.0.1
     tornado.ioloop.IOLoop.current().start()
