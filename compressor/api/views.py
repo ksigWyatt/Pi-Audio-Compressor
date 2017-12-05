7ea64736ae1c5306345884600c70f642fe1a9c33
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-12-05 16:43:25 -0500

    Removing Django files

--- compressor/api/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
